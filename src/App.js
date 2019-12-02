7e147a199f361be5d01c76c006e8f9a0d5974aab
Serhii Babii <babiysergey1988@gmail.com>
2019-12-02 15:25:33 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["render"] }] */
+class App extends Component {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default App;
