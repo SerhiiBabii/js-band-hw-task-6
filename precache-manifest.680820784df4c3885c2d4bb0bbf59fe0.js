5e45f6651973e7d877822a6e14fa96c6fc34edeb
Serhii Babii <babiysergey1988@gmail.com>
2019-12-02 17:40:09 +0200

    Updates

---
+++ precache-manifest.680820784df4c3885c2d4bb0bbf59fe0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bfc744f9188f32a992d0bbf2f2f63fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d08ef3efe9ff6dc2871e",
+    "url": "./static/css/main.79e4e0db.chunk.css"
+  },
+  {
+    "revision": "cdfd8edab5a38fd0bdb3",
+    "url": "./static/js/2.86383252.chunk.js"
+  },
+  {
+    "revision": "d08ef3efe9ff6dc2871e",
+    "url": "./static/js/main.b048c5e8.chunk.js"
+  },
+  {
+    "revision": "efb6b9b76511e7565b67",
+    "url": "./static/js/runtime-main.5cc7a3e4.js"
+  }
+]);
